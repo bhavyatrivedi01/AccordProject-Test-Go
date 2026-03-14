b5bdc68d2d65e354b81023ab0bf158aefc0b8a1e
Bhavya Trivedi <36411614+bhavyatrivedi01@users.noreply.github.com>
2026-03-14 16:19:52 -0600

    Refactor TestAllGeneratedModels for clarity

--- tests/generic_test.go
+++ tests/generic_test.go
@@ -1,45 +1,46 @@
+// We change this to a generic package name because the script 
+// will inject it into the generated folders.
 package main
 
 import (
-    "encoding/json"
-    "reflect"
-    "testing"
+	"encoding/json"
+	"reflect"
+	"testing"
 )
 
-// This test uses reflection to find and test all structs in the package
+// TestAllGeneratedModels now uses reflection to test whatever 
+// structs are available in the current package scope.
 func TestAllGeneratedModels(t *testing.T) {
-    // List of "testable" concepts you expect in your generated code
-    // In a real GSoC project, you could automate this list via 'concerto parse'
-    testCases := []struct {
-        Name  string
-        Value interface{}
-        Data  string
-    }{
-        {
-            Name:  "Shipment", 
-            Value: &Shipment{}, 
-            Data:  `{"shipmentId":"123","weight":10.5}`,
-        },
-    }
+	testCases := []struct {
+		Name  string
+		Value interface{}
+		Data  string
+	}{
+		{
+			// Note: This must match a struct name in your .cto file
+			Name:  "Shipment", 
+			Value: &Shipment{}, 
+			Data:  `{"shipmentId":"123","weight":10.5}`,
+		},
+	}
 
-    for _, tc := range testCases {
-        t.Run(tc.Name, func(t *testing.T) {
-            // Use reflection to verify the type
-            val := reflect.ValueOf(tc.Value)
-            if val.Kind() != reflect.Ptr || val.Elem().Kind() != reflect.Struct {
-                t.Fatalf("%s is not a pointer to a struct", tc.Name)
-            }
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			val := reflect.ValueOf(tc.Value)
+			if val.Kind() != reflect.Ptr || val.Elem().Kind() != reflect.Struct {
+				t.Fatalf("%s is not a pointer to a struct", tc.Name)
+			}
 
-            // Test Unmarshal
-            if err := json.Unmarshal([]byte(tc.Data), tc.Value); err != nil {
-                t.Errorf("Failed to unmarshal %s: %v", tc.Name, err)
-            }
+			// Test Unmarshal
+			if err := json.Unmarshal([]byte(tc.Data), tc.Value); err != nil {
+				t.Errorf("Failed to unmarshal %s: %v", tc.Name, err)
+			}
 
-            // Test Marshal
-            _, err := json.Marshal(tc.Value)
-            if err != nil {
-                t.Errorf("Failed to marshal %s: %v", tc.Name, err)
-            }
-        })
-    }
+			// Test Marshal
+			_, err := json.Marshal(tc.Value)
+			if err != nil {
+				t.Errorf("Failed to marshal %s: %v", tc.Name, err)
+			}
+		})
+	}
 }
